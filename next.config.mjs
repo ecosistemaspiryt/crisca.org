ea92e1095f732f0ac8bab8848b121012d937ad05
consultorametta <consultorametta@gmail.com>
2026-01-08 19:05:09 -0300

    fix: next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     /* config options here */
-    i18n: {
-        locales: ['es', 'pt', 'it'],
-        defaultLocale: 'es',
-        localeDetection: false // Disable Next.js routing, rely on client-side
-    }
 };
 
 export default nextConfig;
